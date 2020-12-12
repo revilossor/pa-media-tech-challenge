5ed3a39ae0712c0a1614a9948de1888dafcdb782
Oliver Ross <oliver.ross01@bbc.co.uk>
2020-12-12 13:18:50 +0000

    add types'ts

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+import { KeyedObject } from './lib/data-store/IDataStore'
+
+export interface Item extends KeyedObject{
+  listKey: string
+  value: string
+  createdAt: string
+  updatedAt: string
+}
+
+export type List = KeyedObject
